1ae85f4a0bf0e73df42dcefbdbf15830928f7bd0
JustValk <50841876+JustValk@users.noreply.github.com>
2022-08-30 15:02:42 +0100

    Update Module.lua

--- Module.lua
+++ Module.lua
@@ -86,7 +86,7 @@ local Aiming = {
         Instance = nil,
         Part = nil,
         Position = nil,
-        Velocity = nil, -- // You might need set the Y velocity to `0` or it bugs out /shrug
+        Velocity = 0, -- // You might need set the Y velocity to `0` or it bugs out /shrug
         OnScreen = false
     }
 }
